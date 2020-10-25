9a55c3771dc526e4850ffb70f584da5b373a01f4
Alx_Bx <cristalexander@gmail.com>
2020-10-24 21:12:43 -0600

    add search

--- parsers/search.rb
+++ parsers/search.rb
@@ -2,6 +2,13 @@ html = Nokogiri.HTML (content)
 products = html.css('.srp-list .s-item')
 
 products.each do |product|
-
+  name = product.css('.s-item__title').text.strip
+  price = product.css('.s-item__price').text.strip
+  url = product.css('.s-item__link').attr('href')
+  pages << {
+    url: url,
+    page_type: 'product'
+  }
 
 end
+
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,3 +2,8 @@ pages << {
     url: 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2047675.m570.l1311&_nkw=nintendo+switch&_sacat=0',
     page_type: 'search'
 }
+
+pages << {
+    url: 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=nintendo+switch&_sacat=0&_pgn=2',
+    page_type: 'search'
+}
